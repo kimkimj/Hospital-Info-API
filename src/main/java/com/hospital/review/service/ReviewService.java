1796573af98132dc9f200b241901bbf1f2780805
u <mjkim2304@gmail.com>
2022-11-25 13:26:19 +0900

    병원 1개 리뷰 조회 기능

--- src/main/java/com/hospital/review/service/ReviewService.java
+++ src/main/java/com/hospital/review/service/ReviewService.java
@@ -44,4 +44,11 @@ public class ReviewService {
                 .message("리뷰 등록이 성공했습니다.")
                 .build();
     }
+
+    public Review getReview(Long id) {
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 id가 없습니다"));
+        return review;
+    }
+
 }
