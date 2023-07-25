faeefe0e39f2f0f83eb26dd08431a3ad1393c2a4
u <mjkim2304@gmail.com>
2023-07-26 03:54:20 +0900

    UPDATE: 컨트롤러 변경

--- src/main/java/com/hospital/review/controller/HospitalController.java
+++ src/main/java/com/hospital/review/controller/HospitalController.java
@@ -4,6 +4,7 @@ import com.hospital.review.domain.Hospital;
 import com.hospital.review.domain.dto.HospitalReadResponse;
 import com.hospital.review.domain.dto.ReviewCreateRequest;
 import com.hospital.review.domain.dto.ReviewCreateResponse;
+import com.hospital.review.domain.dto.ReviewReadResponse;
 import com.hospital.review.service.HospitalService;
 import com.hospital.review.service.ReviewService;
 
@@ -13,31 +14,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/hospitals")
-@RequiredArgsConstructor //@NoArgsConstructor은 지워야한다
-@Slf4j
+@RequiredArgsConstructor
 public class HospitalController {
 
     private final ReviewService reviewService;
     private final HospitalService hospitalService;
 
-
-    // 새로운 리뷰를 등록
-    // client에서 받은 요청을 서비스로 보내고, 서비에서 return바은 것을 사용자에서 response로 돌려보낸다
-    @PostMapping("/{id}/reviews")
-     public ResponseEntity<ReviewCreateResponse> add(@RequestBody ReviewCreateRequest reviewCreateRequest) {
-        log.info("{}", reviewCreateRequest);
-        return ResponseEntity.ok().body(reviewService.createReview(reviewCreateRequest));
-    }
-
-    // 병원 id를 조회하면 리뷰까지 같이 나오는 기능
-    // api/v1/hospitals/{id}
+    // 병원 id를 조회하면 병원 정보와 리뷰 조회
     @GetMapping("/{id}")
     public ResponseEntity<HospitalReadResponse> getHospital(@PathVariable Long id) {
         Hospital hospitalResponse = hospitalService.findById(id);
         HospitalReadResponse hospitalReadResponse = HospitalReadResponse.fromEntity(hospitalResponse);
         return ResponseEntity.ok().body(hospitalReadResponse);
+    }
+
+    //병원 ID로 해당 병원의 모든 리뷰 조회
+    @GetMapping("/{hospitalId}/reviews")
+    public ResponseEntity<List<ReviewReadResponse>> reviews(@PathVariable Long hospitalId){
+        return ResponseEntity.ok().body(reviewService.findAllByHospitalId(hospitalId));
     }
 }
--- src/main/java/com/hospital/review/controller/ReviewController.java
+++ src/main/java/com/hospital/review/controller/ReviewController.java
@@ -1,16 +1,15 @@
 package com.hospital.review.controller;
 
 import com.hospital.review.domain.Review;
+import com.hospital.review.domain.dto.ReviewCreateRequest;
+import com.hospital.review.domain.dto.ReviewCreateResponse;
 import com.hospital.review.domain.dto.ReviewReadResponse;
 import com.hospital.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,21 +20,7 @@ import java.util.List;
 public class ReviewController {
     private final ReviewService reviewService;
 
-    // 리뷰 ID로 리뷰 1개 조회
-    // GET /api/v1/reviews/{id}
-    /*
-    @GetMapping("/{id}")
-    public ResponseEntity<ReviewReadResponse> get(@PathVariable Long id) {
-        Review review = reviewService.getReview(id);
-        ReviewReadResponse response = ReviewReadResponse.builder()
-                .id(review.getId())
-                .title(review.getTitle())
-                .content(review.getContent())
-                .patientName(review.getPatientName())
-                .hospitalName(review.getHospital().getHospitalName())
-                .build();
-        return ResponseEntity.ok().body(response);
-    }*/
+    // 리뷰 ID로 조회
     @GetMapping("/{id}")
     public ResponseEntity<ReviewReadResponse> get(@PathVariable Long id) {
         Review review = reviewService.getReview(id);
@@ -43,10 +28,11 @@ public class ReviewController {
         return ResponseEntity.ok().body(response);
     }
 
-    //병원 ID로 해당 병원의 모든 리뷰 죄회하는 기능
-    // GET /api/v1/reviews/{hospitalId}/reviews
-    @GetMapping("/{hospitalId}/reviews")
-    public ResponseEntity<List<ReviewReadResponse>> reviews(@PathVariable Long hospitalId){
-        return ResponseEntity.ok().body(reviewService.findAllByHospitalId(hospitalId));
+    // 리뷰 등록
+    @PostMapping("/{id}")
+    public ResponseEntity<ReviewCreateResponse> add(@RequestBody ReviewCreateRequest reviewCreateRequest) {
+        log.info("reviewCreateRequest={}", reviewCreateRequest);
+        return ResponseEntity.ok().body(reviewService.createReview(reviewCreateRequest));
     }
+
 }
