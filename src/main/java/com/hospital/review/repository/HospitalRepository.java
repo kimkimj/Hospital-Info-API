e24631ef3560df6f39ca56a71db20b3c81a3988e
u <mjkim2304@gmail.com>
2023-07-26 04:02:35 +0900

    UPDATE: hospitalRepository 변경

--- src/main/java/com/hospital/review/repository/HospitalRepository.java
+++ src/main/java/com/hospital/review/repository/HospitalRepository.java
@@ -3,5 +3,11 @@ package com.hospital.review.repository;
 import com.hospital.review.domain.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> types);
+    List<Hospital> findByHospitalNameContaining(String hospitalName);
+
+    List<Hospital> findByHospitalName(String hospitalName);
 }
