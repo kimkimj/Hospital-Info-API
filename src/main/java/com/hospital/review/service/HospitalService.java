b666c97591aa40c744902b9fd40ca95a45c1ceac
u <mjkim2304@gmail.com>
2023-07-29 07:13:49 +0900

    UPDATE: service

--- src/main/java/com/hospital/review/service/HospitalService.java
+++ src/main/java/com/hospital/review/service/HospitalService.java
@@ -1,6 +1,7 @@
 package com.hospital.review.service;
 
-import com.hospital.review.domain.Hospital;
+import com.hospital.review.domain.dto.HospitalResponse;
+import com.hospital.review.domain.entity.Hospital;
 import com.hospital.review.repository.HospitalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,20 @@ import org.springframework.stereotype.Service;
 public class HospitalService {
     private final HospitalRepository hospitalRepository;
 
-    public Hospital findById(Long id) {
+    public HospitalResponse findById(Integer id) {
         Hospital hospital = hospitalRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("id가 없습니다"));
-        return hospital;
+                .orElseThrow(() -> new IllegalArgumentException("id가 없습니다"));
+        HospitalResponse hospitalResponse = Hospital.of(hospital);
+
+        if (hospital.getBusinessStatusCode() == 13) {
+            hospitalResponse.setBusinessStatusName("영업중");
+        } else if (hospital.getBusinessStatusCode() == 3) {
+            hospitalResponse.setBusinessStatusName("폐업");
+        } else {
+            hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusinessStatusCode()));
+        }
+
+        return hospitalResponse;
     }
+
 }
