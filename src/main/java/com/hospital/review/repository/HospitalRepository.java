a591b6f72f7021aa671289b07fa6c86685a2800e
u <mjkim2304@gmail.com>
2023-07-29 07:13:25 +0900

    UPDATE: repository

--- src/main/java/com/hospital/review/repository/HospitalRepository.java
+++ src/main/java/com/hospital/review/repository/HospitalRepository.java
@@ -1,13 +1,21 @@
 package com.hospital.review.repository;
 
-import com.hospital.review.domain.Hospital;
+import com.hospital.review.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-    List<Hospital> findByBusinessTypeNameIn(List<String> types);
-    List<Hospital> findByHospitalNameContaining(String hospitalName);
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 
-    List<Hospital> findByHospitalName(String hospitalName);
+    // 키워드 검색, 페이징 x
+    List<Hospital> findByRoadNameAddressContaining(String keyword);
+    List<Hospital> findByHospitalNameStartingWith(String keyword);
+    List<Hospital> findByHospitalNameEndsWith(String keyword);
+    List<Hospital> findByPatientRoomCountBetween(int keyword1, int keyword2);
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b);
+
+    // 키워드 검색, 페이징
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable);
 }
