d9bc42cddaa28e16104244226ce5b460f677300e
u <mjkim2304@gmail.com>
2023-07-29 08:48:49 +0900

    UPDATE: new properties to Hospital entity

--- src/main/java/com/hospital/review/domain/entity/Hospital.java
+++ src/main/java/com/hospital/review/domain/entity/Hospital.java
@@ -10,32 +10,45 @@ import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class Hospital {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(name = "road_name_address")
     private String roadNameAddress;
-
     @Column(name = "hospital_name")
     private String hospitalName;
+    private String phoneNumber;
     private Integer patientRoomCount;
     private Integer totalNumberOfBeds;
     private String businessTypeName;
-    private Float totalAreaSize;
     private Integer businessStatusCode;
+    @Column(name = "medical_department")
+    private String medicalDepartment;
+
+    public Hospital(String roadNameAddress, String hospitalName, String phoneNumber, Integer patientRoomCount,
+                    Integer totalNumberOfBeds, String businessTypeName, Integer businessStatusCode,
+                    String medicalDepartment) {
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.phoneNumber = phoneNumber;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.businessStatusCode = businessStatusCode;
+        this.medicalDepartment = medicalDepartment;
+    }
 
     // HospitalEntity를 HospitalResponse(Dto)로 변환
     public static HospitalResponse of(Hospital hospital) {
         return new HospitalResponse(hospital.getId(),
                 hospital.getHospitalName(),
+                hospital.getPhoneNumber(),
                 hospital.getRoadNameAddress(),
                 hospital.getPatientRoomCount(),
                 hospital.getTotalNumberOfBeds(),
                 hospital.getBusinessTypeName(),
-                hospital.getTotalAreaSize());
+                hospital.getMedicalDepartment());
     }
 }
