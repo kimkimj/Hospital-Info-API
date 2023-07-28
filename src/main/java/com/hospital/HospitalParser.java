718d8a9e6bf939f5b8f9fe2f3a7765fd6f8aac7a
u <mjkim2304@gmail.com>
2023-07-29 08:48:18 +0900

    ADD: HospitalParser

---
+++ src/main/java/com/hospital/HospitalParser.java
@@ -0,0 +1,59 @@
+package com.hospital;
+
+import com.hospital.review.domain.entity.Hospital;
+
+public class HospitalParser {
+    public Hospital parse(String str) {
+        //탭으로 구분된 txt파일을 split 한다.
+        String[] row = str.split("\t");
+
+        //개방 서비스명 (의원 혹은 병원)
+        String openServiceName = row[1];
+
+        //영업상태 구분 (1: 영업/정상  || 2: 휴업 || 3: 폐업 || 4: 취소/말소/만료/정지/중지)
+        Integer businessStatusCode = Integer.parseInt(row[7]);
+
+        //소재지 전화
+        String phoneNumber = row[15];
+
+        //도로명 주소
+        String roadNameAddress = row[19].replaceAll("\"", "");
+        //사업장명
+        String hospitalName = row[21];
+        //업태구분명
+        String businessTypeName = row[25];
+
+        //병상 수
+        Integer totalNumberOfBeds = 0;
+
+        //입원실 수
+        Integer patientRoomCount = 0;
+
+        //진료과목
+        String medicalDepartment = null;
+
+
+        if (row.length >= 30) {
+            if (!row[30].equals(""))
+                patientRoomCount = Integer.parseInt(row[30]);
+        }
+        if (row.length >= 31) {
+            if (!row[31].equals(""))
+                totalNumberOfBeds = Integer.parseInt(row[31]);
+        }
+
+        if (row.length >= 34) {
+            if (!row[34].equals("")) {
+                medicalDepartment = row[34].replaceAll("\"", "");
+            }
+        }
+
+        //가져온 값들을 Hospital 객체에 넣는다
+        Hospital hospital = new Hospital(
+                 roadNameAddress, hospitalName, phoneNumber, patientRoomCount,
+                 totalNumberOfBeds, businessTypeName, businessStatusCode,
+                 medicalDepartment);
+
+        return hospital;
+    }
+}
